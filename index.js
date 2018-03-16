b478823c3898d72b0bf63c81ce245145f32c2e18
hrozman <hamsyari.rozman@ecofactor.com>
2018-03-16 10:30:53 -0700

    Edit javascript, set button to disabled initially

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ function verifyCompatible(){
 }
 
 function toggleInputEditable(value) {
-    console.log("hello");
-    document.getElementById('inputNumOfThermostats').disabled = value;
-    document.getElementById('inputNumOfCWireAdapters').disabled = value;
-    document.getElementById('inputThermostatColor').disabled = value;
-    document.getElementById('inputHasWifi').disabled = value;
-    document.getElementById('inputHasAC').disabled = value;
-    document.getElementById('inputFirstName').disabled = value;
-    document.getElementById('inputLastName').disabled = value;
-    document.getElementById('inputAddress1').disabled = value;
-    document.getElementById('inputAddress2').disabled = value;
-    document.getElementById('inputCity').disabled = value;
-    document.getElementById('inputState').disabled = value;
-    document.getElementById('inputZip').disabled = value;
-    document.getElementById('inputPhone').disabled = value;
-    document.getElementById('inputEmail').disabled = value;
-    document.getElementById('inputUserFeedback').disabled = value;
-    document.getElementById('checkTC').disabled = value
+    // document.getElementById('inputNumOfThermostats').disabled = value;
+    // document.getElementById('inputNumOfCWireAdapters').disabled = value;
+    // document.getElementById('inputThermostatColor').disabled = value;
+    // document.getElementById('inputHasWifi').disabled = value;
+    // document.getElementById('inputHasAC').disabled = value;
+    // document.getElementById('inputFirstName').disabled = value;
+    // document.getElementById('inputLastName').disabled = value;
+    // document.getElementById('inputAddress1').disabled = value;
+    // document.getElementById('inputAddress2').disabled = value;
+    // document.getElementById('inputCity').disabled = value;
+    // document.getElementById('inputState').disabled = value;
+    // document.getElementById('inputZip').disabled = value;
+    // document.getElementById('inputPhone').disabled = value;
+    // document.getElementById('inputEmail').disabled = value;
+    // document.getElementById('inputUserFeedback').disabled = value;
+    // document.getElementById('checkTC').disabled = value;
+
+    document.getElementById('btnLinkUtilityAccount').disabled = value;
+    document.getElementById('btn_signup_enroll').disabled = value;
+    document.getElementById('btn_signup_cancel').disabled = value;
 }
